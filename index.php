1e54cebcb55c36cbbe41add30b0e4a1e331b8368
Mark Edriane Nolledo <mark.nolledo@gmail.com>
2024-09-01 13:59:45 -0500

    Created head component

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php
     require('./php/app.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo(APP_NAME); ?> - A Customizable Quiz Generator</title>
-    <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="./assets/css/fontawesome/fontawesome.min.css"/>
-    <link rel="stylesheet" type="text/css" href="./assets/css/fontawesome/brands.min.css"/>
-    <link rel="stylesheet" type="text/css" href="./assets/css/fontawesome/solid.min.css"/>
-    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
-</head>
+<?php require('./php/html/html.head.php'); ?>
 <body>
     <?php include('./php/components/navbar.php'); ?>
     <div class="container col-12 col-xxl-8">
---
+++ php/html/html.head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo (APP_NAME); ?> - A Customizable Quiz Generator</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/fontawesome/fontawesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/fontawesome/brands.min.css" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/fontawesome/solid.min.css" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+</head>
